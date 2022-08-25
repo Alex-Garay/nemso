94cedc8e5f4962134d7df6c6f9b2d78f56b0825e
Alex Garay <alex.garay94@gmail.com>
2022-08-25 11:18:23 -0700

    login page UI finished

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,12 +1,22 @@
 import { useState } from "react";
 import { NextPage } from "next";
-import { Button, Input, Typography } from "@material-tailwind/react";
+import Link from "next/link";
+import {
+  Button,
+  Input,
+  Typography,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Checkbox,
+} from "@material-tailwind/react";
 const LoginPage: NextPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   return (
-    <div>
-      <form
+    <div className="flex flex-col h-screen justify-center items-center">
+      {/* <form
         onSubmit={async function handleSubmit(event) {
           event.preventDefault();
 
@@ -25,10 +35,10 @@ const LoginPage: NextPage = () => {
             console.error("An unexpected error happened:", error);
           }
         }}
-        className="w-64 m-5"
+        className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100"
       >
         <Typography variant="h5" color="blue-gray">
-          Login
+          Welcome back, signin!
         </Typography>
         <Input
           label="username"
@@ -42,6 +52,53 @@ const LoginPage: NextPage = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
         <Button type="submit">Login</Button>
+      </form> */}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log(username, password);
+        }}
+      >
+        <Card className="w-96">
+          <div className="text-center">
+            <Typography variant="h6" color="blue-gray">
+              Welcome back, signin!
+            </Typography>
+          </div>
+          <CardBody className="flex flex-col gap-4">
+            <Input
+              label="Email"
+              size="lg"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+              label="Password"
+              size="lg"
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+            />
+            <div className="-ml-2.5">
+              <Checkbox label="Remember Me" />
+            </div>
+          </CardBody>
+          <CardFooter className="pt-0">
+            <Button variant="gradient" fullWidth color="indigo" type="submit">
+              Sign In
+            </Button>
+            <Typography variant="small" className="mt-6 flex justify-center">
+              Don't have an account?
+              <Typography
+                as="a"
+                href="#signup"
+                variant="small"
+                color="blue"
+                className="ml-1 font-bold"
+              >
+                Sign up
+              </Typography>
+            </Typography>
+          </CardFooter>
+        </Card>
       </form>
     </div>
   );
--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -9,8 +9,8 @@ const SignUpPage: NextPage = () => {
 
   // Graphql query to find if the username already exists in the database
   const FIND_EXISTING_USER = gql`
-    query {
-      users(where: { username: "${username}" }) {
+    query Users($username: String!) {
+      users(where: { username: $username }) {
         id
         username
         password
@@ -20,28 +20,32 @@ const SignUpPage: NextPage = () => {
   // Using regular useQuery throws an error about hook rule violations, HAVE to use useLazyQuery:
   // useLazyQuery will run everytime username or password state changes.
   // Refer to: https://www.apollographql.com/docs/react/api/react/hooks/#uselazyquery
-  const [validateExisting, { called, loading, data }] =
-    useLazyQuery(FIND_EXISTING_USER);
+  const [validateExisting, { called, loading, data }] = useLazyQuery(
+    FIND_EXISTING_USER,
+    {
+      onCompleted: (data) => {
+        console.log("Completed");
+      },
+    }
+  );
   // Checks the returned data to check if a user exists
   if (data && data.users.length !== 0) {
     console.log("FOUND AN EXISTING USER");
   }
-  // Runs each time our useLazyQuery hook runs
-  if (called) {
-    console.log("ITS BEING CALLED");
-  }
   return (
-    <div>
+    <div className="flex justify-center items-center flex-col h-screen bg-gray-100">
+      <Typography variant="h3" color="blue-gray">
+        Welcome to Nemso
+      </Typography>
       <form
         onSubmit={(event) => {
           event.preventDefault();
-          validateExisting();
+          validateExisting({
+            variables: { username: username },
+          });
         }}
-        className="w-64 m-5"
+        className="w-64"
       >
-        <Typography variant="h5" color="blue-gray">
-          Register
-        </Typography>
         <Input
           label="username"
           type="text"
@@ -54,11 +58,11 @@ const SignUpPage: NextPage = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
         {data && data.users.length !== 0 ? (
-          <Typography variant="h8" color="red">
+          <Typography variant="small" color="red">
             User Already Exists
           </Typography>
         ) : null}
-        <Button type="submit">Login</Button>
+        <Button type="submit">Signup</Button>
       </form>
     </div>
   );
